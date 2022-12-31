a66cfb33f04b539ce0680d512e959deeff3a9123
Rodionov Roman <rodionov2296@gmail.com>
2022-12-31 12:57:49 +0400

    fix _events property

--- src/packages/domain/entity.ts
+++ src/packages/domain/entity.ts
@@ -3,29 +3,37 @@ export interface Event {
     type: string;
 }
 
+const EVENTS = Symbol('events');
+
 class Entity<E extends Event> {
-    private _events: E[] = [];
+    private [EVENTS]!: E[];
 
     get events(): ReadonlyArray<E> {
         // eslint-disable-next-line no-underscore-dangle
-        return this._events || [];
+        return this[EVENTS] || [];
     }
 
     // eslint-disable-next-line no-useless-constructor
-    protected constructor(readonly id: string) {}
+    protected constructor(readonly id: string) {
+        Object.defineProperty(this, EVENTS, {
+            value: [],
+            writable: true,
+            enumerable: false
+        });
+    }
 
     protected pushEvent(event: E) {
         // We use MikroORM. It doesn't creates instances by Object.create
         // so setting default value "_event = []" doesn't work
         // It's possible to pass forceCOnstructor: true, but it produces many more bugs
         // eslint-disable-next-line no-underscore-dangle
-        if (this._events === undefined) {
+        if (this[EVENTS] === undefined) {
             // eslint-disable-next-line no-underscore-dangle
-            this._events = [];
+            this[EVENTS] = [];
         }
 
         // eslint-disable-next-line no-underscore-dangle
-        this._events.push(event);
+        this[EVENTS].push(event);
     }
 }
 
